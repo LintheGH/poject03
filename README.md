ad57e0ecd503969f8d3334d56d4865850194bf06
linthegh <1454691799@qq.com>
2018-08-16 10:54:00 +0800

    some fixed

---
+++ README.md
@@ -0,0 +1,4 @@
+#### project demo 
+- 创建项目
+- 安装依赖包
+  - node-sass sass-loader react-router-dom react-redux redux antd 
---
+++ src/Components/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+class App extends React.Component {
+
+  render () {
+    return (
+      <div className="app">
+        a's'd'fasdf
+      </div>
+    )
+  }
+}
+export default App
